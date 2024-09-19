fdc2db25fc50474a60acc8895d33085d51b6f1ba
tarek <tarekibrahim3@cmail.carleton.com>
2024-09-19 12:21:23 -0400

    first commit

---
+++ src/BuddyInfo.java
@@ -0,0 +1,11 @@
+public class BuddyInfo {
+    public BuddyInfo(String name) {
+        this.name = name;
+    }
+
+    private String name;
+    public static void main(String[] args) {
+        BuddyInfo buddy = new BuddyInfo("Homer");
+        System.out.println("Hello world!");
+    }
+}
