92ddef9d723e7ccfc510ed805e0db0459a7cd391
tarek <tarekibrahim3@cmail.carleton.com>
2024-09-19 12:26:14 -0400

    added more in main()

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -25,5 +25,14 @@ public class AddressBook {
 
     public static void main(String[] args) {
         System.out.println("Address Book");
-    }
+        AddressBook addressBook = new AddressBook();
+        BuddyInfo buddy1 = new BuddyInfo("Buddy1");
+        BuddyInfo buddy2 = new BuddyInfo("Buddy2");
+        BuddyInfo buddy3 = new BuddyInfo("Buddy3");
+
+        addressBook.addBuddy(buddy1);
+        addressBook.addBuddy(buddy2);
+        addressBook.addBuddy(buddy3);
+        addressBook.removeBuddy(buddy1);
+        addressBook.printBuddies();    }
 }
